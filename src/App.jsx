7e06f895a464fb7a1fd71a8b327e6e394d0acad8
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-08-19 13:18:59 +0500

    3-dars vazifasi

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DynamicForm from './components/ynamicForms/DynamicForms'
+function App() {
+  return (
+    <div style={{maxWidth: "1200px", margin: "0 auto"}}>
+      <DynamicForm/>
+    </div>
+  )
+}
+
+export default App
